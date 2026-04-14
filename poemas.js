9114ff67032900f8b0bfd424553dd6236be387c1
R41N13R0 <n35qu1k3r@gmail.com>
2026-04-14 17:28:42 +0200

    poemas aparte, 3 poemas mas

---
+++ poemas.js
@@ -0,0 +1,329 @@
+
+      var poemas = [
+        {
+          titulo: "Un soneto me manda hacer Violante",
+          poema: `
+            Un soneto me manda hacer Violante,
+            que en mi vida me he visto en tal aprieto;
+            catorce versos dicen que es soneto:
+            burla burlando van los tres delante.
+
+            Yo pensé que no hallara consonante,
+            y estoy a la mitad de otro cuarteto;
+            mas si me veo en el primer terceto,
+            no hay cosa en los cuartetos que me espante.
+
+            Por el primer terceto voy entrando,
+            y parece que entré con pie derecho,
+            pues fin con este verso le voy dando.
+
+            Ya estoy en el segundo, y aun sospecho
+            que estoy los trece versos acabando;
+            contad si son catorce, y está hecho.`,
+          autor: "— Lope de Vega",
+          pregunta: "¿Por qué este poema?",
+          porque: "Este poema es el primero que leí siendo algo consciente. Me mandaron a comentar o analizar un poema con 12 años y escogí este. Desde entonces, el soneto es mi estilo de poema favorito y al que accedo siempre como primera opción."
+        },
+        {
+          titulo: "Oda nº1",
+          poema: `
+            En un ruidoso pasillo acabó
+            lo que bonito estuvo siendo y era,
+            lo que desde poco antes quisiera,
+            lo que el pasado sábado empezó.
+
+            Mi yo del pasado no lo soñó,
+            mas en esta la nueva primavera
+            y sin que ni yo mismo lo supiera,
+            el viaje a Eastbourne a mí me cambió.
+
+            Entendí que lo mío no es lo único
+            y que enfocarse sin tener opciones
+            puede llegar a hacerte ser un básico.
+
+            Pero mi brillo asciende con mis sones.
+            Olivares posee un brillo mágico
+            y es el que une nuestros corazones.`,
+          autor: "— Rainiero López Vargas 9/4/2025",
+          pregunta: "¿Por qué este poema?",
+          porque: "Esto lo hice a modo de oda al viaje que hice a Eastbourne. La simplicidad que posee para mi nivel en ese momento se debió a la falta de recursos tales como tiempo, internet para referencias o simplemente un diccionario. Esta carencia se debe a su vez al hecho de que lo escribí estando montado en el avión. Me hubiera dado tiempo a mejorarlo un poco, pero me mareé y lo dejé así."
+        },
+        {
+          titulo: "Plan B",
+          poema: `
+            Con este título uno se espera
+            que esto vaya de encontrar otra forma
+            de conseguir lo que pasada norma
+            no pudo, pero de otra manera.
+
+            Nada se aleja más en esta era.
+            Pues en buen punto de inflexión torna
+            mi vida, que la anterior deforma;
+            como la muerte a esta mera.
+
+            Mucho de mi camino me he alejado.
+            Pues no es el fin el que indica el camino,
+            ni tampoco el camino atravesado.
+
+            Es el camino, el que con hilo fino
+            te mostrará hasta el que te ha guiado
+            tu tejida historia en el camino
+
+
+            Dejando estás cosas aparte
+            y sin aún saber si tú quieres formar parte,
+            te ofrezco conmigo llevarte a marte,
+            para allí volverte a decir que eres una obra de arte
+
+            Dicho esto frente al toro me alzo;
+            Como buen nazareno: firme pero descalzo,
+            y con confianza, como en un abrazo;
+            aprovechando que aún estamos en marzo.
+
+            Al final del día, cuando oscureció;
+            tras horas y horas donde el sol le dió;
+            en mitad de la noche azul se prendió
+            una vela que el mundo entero iluminó.
+
+            Pudiendo un bosque hacer arder en llamas,
+            esa vela se reprime y aguanta hasta la siguiente mañana
+            para asegurarse de que lo que ama
+            podrá aguantar semejante flama.
+
+            Pasión, dolor que la sangre me arranca.
+            Cirios que me guían entre nieve y azahares
+            a un corazón en el corazón de Olivares`,
+          autor: "— Rainiero López Vargas 13/3/2026",
+          pregunta: "¿Por qué este poema?",
+          porque: "La importancia de este poema no es ni más ni menos que por ser el primero que he recitado a la persona a la que va dirigido. Lo escribí en clases de matemáticas y en mi casa. La primera parte, abstracta, parece una absurda explicación del título, pero refleja más que eso. la segunda es orientada al 100% al objetivo del poema: demostrar mi amor."
+        },
+        {
+          titulo: "B(/(missing)N)",
+          poema: `
+            Claro que siento.
+            Claro que quiero.
+            Pero no me siento pleno
+            sin ser plenamente sincero
+
+            No estoy loco por ella, 
+            sino loco, pero con amor a ella.
+            No es que sea la primera opción ni que no lo sea.
+            Para haber una primera debe haber una segunda.
+            Es la mejor, no por comparación,
+            sino por buena.
+            Es la que me aporta felicidad,
+            la que es simple sinceridad;
+            en la que no tendría que actuar
+            para no perder lo que quiero,
+            sino acostumbrarme a actuar como deseo
+
+            Nada es eterno, ella lo sabe.
+            Pero, si nosotros no somos eternos tampoco,
+            ¿por qué deberíamos creer que dura menos que nosotros?
+            ¿por qué el amor vive menos que un hombre?
+
+            No fuerzo, intento.
+            Quiero, lucho, peleo, no noqueo.
+            Me limito al máximo.
+            Y me sorprende.
+            Me sorprende que lo aguante,
+            que me aguante.
+            Pero me sorprende mal. ¿Mal?
+            Porque me gusta y me descentra.
+            Me acerca más al límite
+            y debo esforzarme más en no sobrepasarlo.
+            Eso agota y entristece;
+            A su vez desilusiona.
+            Pero ni el amor con esperanza,
+            ni esta desilusión
+            son generados por ella a consciencia,
+            sino por mí, sin intención ni necesidad.
+            Un bien amargo y frio.
+            Un mal que me abraza y alegra.
+            Lesión del alma que se tira.
+            Dolor automático que busca placer
+            y por el placer se crea,
+            aunque con el placer no sé si se destruye.
+            Placer.
+            No omnipotente.
+            No genera placer;
+            solo genera dolor,
+            dolor pasajero,
+            intenso.
+            Dolor.
+            Sin necesidad.
+            Sin utilidad
+            Sin fin,
+            pero presente;
+            sobretodo en el pasado.
+            Y como parece mayor el dolor anterior,
+            creemos que luego será menor.
+            lo mismo pasa con el placer,
+            pero esperamos que vaya a mejor.
+            No espero que disminuya mi dolor.
+            Quiero que desaparezca.
+            Sin embargo, espero que aumente mi placer
+            porque lo veo en cero,
+            solo puede subir.
+            Iluso de mí, ¿no?`,
+          autor: "— Rainiero López Vargas 17/4/2026",
+          pregunta: "¿Por qué este poema?",
+          porque: "Simplemente es el primero que hago sin rima. Escribiendo sentimientos, sin intentar aplicarles una formula ni insertarlos en un marco."
+        },
+        {
+          titulo: "Tú.",
+          poema: `
+            Si yo fuera Eladio Carrión,
+            te diría que me gustas natural.
+            Pero como no lo soy
+            me lo callo por vergüenza.
+            
+            Lo que ofreces no lo sé.
+            Tampoco pasa al revés.
+            No nos conocemos en relación.
+            Pero es parte de la experiencia, ¿no?
+            
+            Creemos que nos conocemos,
+            que como a libro aboierto nos leemos;
+            pero no vemos más
+            que lo que el otro nons deja ver.
+            
+            Sea por decisión propia
+            o por causas ajenas,
+            lo que sabemos del otro
+            puede ser o no ser.
+            
+            Sin embargo no se sabe
+            de uno mismo lo que él quiere.
+            Y lo malo de eso
+            Es que no suele ser bueno
+            
+            Me sorprende y me gusta
+            que por ti asoma una luz
+            que te esfuerzas en esconder
+            sin saber que es el bien en tí.
+            
+            Tus superlativos esfuerzos
+            mucho me extrañan.
+            Porque lo que ocultas por tenerlo
+            es lo que me asombra de ti.
+            
+            Lo que ni tú ves
+            y pocos podemos.
+            Es lo que me atrae.
+            Pero lo que crees que te mueve
+            y lo que se ve simple
+            es lo que me aleja.
+            No porque lo elija,
+            sino porque todos lo creen;
+            y a martillazos se matan las ideas.
+            
+            Si simplificara mis ideas no sería yo.
+            ¿Conseguiría lo que quiero? Tal vez.
+            Pero me prefiero así
+            
+            Esta es mi virtud que quiero que conozcas.
+            Porque ves lo que es, aunque no lo entiendas.
+            Y lo que ves no es el por qué, que es la verdad;
+            verdad tuya que me gusta: por qué.`,
+          autor: "— Rainiero López Vargas 9/4/2026",
+          pregunta: "¿Por qué este poema?",
+          porque: "No tiene un por qué como los otros. Es uno más. Pero unos más que no debe ser recordado como 'uno más'. (Curiosamente, coincide con la fecha de la Oda nº1)."
+        },
+        {
+          titulo: "Luces y sombras",
+          poema: `
+            El amor ciego
+            no produce ceguera;
+            solo cambia el punto de vista
+            a uno, con mucha luz.
+            Luz que te deja ver,
+            pero te oculta lo que hay frente a tí.
+            Te deslumbra en la oscuridad.
+            Oscuridad de la vida.
+            
+            Son nuestros ojos los que buscan la oscuridad,
+            orcuridad que cura;
+            pero en el camino tropiezan
+            con ellos mismos.
+            Con ellos porque son cristales;
+            cristales que perturban la luz,
+            amplificando su poder.
+            Cristales que olvidan la oscuridad,
+            que se alejan de nosotros.
+            Ocultan la oscuridad de la luz.
+            
+            Tras los cristales y la luz
+            está el yo pensante,
+            el que debe interpretar
+            lo que los ojos le ofrecen.
+            
+            Es fácil ver la luz.
+            Es cómodo y simple.
+            Es intuitivo y peligroso.
+            Es reconfortable y malicioso.
+            A todos gusta la luz,
+            porque muestra los bonitos objetos.
+            Les inquieta la oscuridad,
+            porque solo usan cristales.
+            
+            Pero la verdad no es la luz.
+            Tampoco lo es la oscuridad.
+            Es el conjunto, pero no.
+            No es la suma ni la diferencia.
+            Es la contradicción en sí.
+            
+            De la luz no sale nada feo;
+            todo ahí es bello.
+            No solo lo parece, lo es.
+            No pasa eso en la oscuridad.
+            Ahí se encuentran
+            bello y horrendo.
+            Pero sobretodo destacar
+            que ahí se encuentra
+            en su mayor parte la verdad.
+            Y también es donde lo bello suele estar.
+            Todo de la luz es bello,
+            pero casi todo lo bello en la oscuridad suele estar.
+            
+            Lo malo de esto es la interpretación;
+            por lo fácil que es ver la luz,
+            olvidando la oscuridad.
+            Lo cómodo que es.
+            Y lo bonito que parece.
+            Pero deja un vacío.
+            Un vacío que no se puede llenar de luz,
+            que necesita oscuridad.
+            Un vacío de verdad,
+            que es mucho mayor que la luz.
+            
+            En su parte, la oscuridad define.
+            Define cómo es la verdad.
+            Define dónde y cuándo.
+            Incluso es capaz de definir
+            el por qué y la finalidad.
+            Comparte con la luz
+            lo bello y lo bueno.
+            Pero saca lo puro, mejor y más bello.`,
+          autor: "— Rainiero López Vargas 13/4/2026",
+          pregunta: "¿Por qué este poema?",
+          porque: "Alegoría. Simple, completa, detallada. Sin terminar, pero con punto y final."
+        }
+        /*
+        Para añadir poemas:
+        en el último "}""
+        se añade una ","
+        y sería
+        {
+        titulo: "Título del poema",
+        poema: `
+          Verso uno
+          verso 2
+          
+          verso 3`,
+        autor: "— Nombre, fecha",
+         pregunta: "¿Por qué este poema?",
+         porque: "Explicación personal..."
+        }
+        */
+      ];
